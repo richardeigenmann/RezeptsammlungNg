c189f9d6d2e320c5e11cd8896fa31d8e64c97858
richardeigenmann <richard.eigenmann@gmail.com>
2018-05-28 00:20:52 +0200

    Put app on gh-pages

--- src/app/services/recipe-site.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class RecipeSiteService {
-
-    public getRecipeSite(): string { return 'http://richieigenmann.users.sourceforge.net'; }
-
-}
--- src/app/shared/encodeUri.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'encodeURI' })
-
-export class EncodeURI implements PipeTransform {
-    transform(value: string) {
-        return encodeURI(value);
-    }
-}
