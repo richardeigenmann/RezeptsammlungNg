80bc02dfd989b3079898dab10dbdd08cdfe432f9
Richard Eigenmann <richard.eigenmann@gmail.com>
2020-03-26 00:26:38 +0100

    Fix Hamburger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-recipe-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
