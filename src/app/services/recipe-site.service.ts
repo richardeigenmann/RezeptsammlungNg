b499777fd90125b2b56fe8f20ea9130ba000c846
Richard Eigenmann <richard.eigenmann@gmail.com>
2020-10-24 13:43:12 +0200

    Switch to Github

--- src/app/services/recipe-site.service.ts
+++ src/app/services/recipe-site.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RecipeSiteService {
 
-    public getRecipeSite(): string { return 'http://richieigenmann.users.sourceforge.net'; }
-    // public getRecipeSite(): string { return 'https://richardeigenmann.github.io/Rezeptsammlung'; }
+    // problem: not https
+    // public getRecipeSite(): string { return 'http://richieigenmann.users.sourceforge.net'; }
+    public getRecipeSite(): string { return 'https://richardeigenmann.github.io/Rezeptsammlung'; }
     public getRecipesUrl(): string { return './api/products/recipes.json'; }
 
 }
