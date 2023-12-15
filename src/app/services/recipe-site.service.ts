c7e197b63cabe9eb70f5de326cc804e07eaec52c
Richard Eigenmann <richard.eigenmann@gmail.com>
2023-12-15 16:34:32 +0100

    Do away with local recipes

--- src/app/services/recipe-site.service.ts
+++ src/app/services/recipe-site.service.ts
@@ -4,12 +4,6 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class RecipeSiteService {
-
-    // problem: not https
-    // public getRecipeSite(): string { return 'http://richieigenmann.users.sourceforge.net'; }
     public getRecipeSite(): string { return 'https://richardeigenmann.github.io/Rezeptsammlung'; }
-    //public getRecipesUrl(): string { return './api/products/recipes.json'; }
     public getRecipesUrl(): string { return 'https://richardeigenmann.github.io/Rezeptsammlung/recipesArray.json'; }
-    
-
 }
