940daf83f9e3b59624ffd620f37c85f237ba6f15
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-03-16 10:11:15 +0100

    Update Timestamp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ declare const require: any;
 
 export const environment = {
   production: true,
-  buildTimeStamp: 'Monday, 10 March 2025 23:35:13 CET',
+  buildTimeStamp: 'Sunday, 16 March 2025 10:09:53 CET',
   apiEndpoint: 'https://richardeigenmann.github.io/Rezeptsammlung/recipesArray.json',
   appVersion: require('../../package.json').version,
   angularVersion: require('../../package.json').dependencies['@angular/core'],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ declare const require: any;
 
 export const environment = {
   production: false,
-  buildTimeStamp: 'Monday, 10 March 2025 23:35:13 CET',
+  buildTimeStamp: 'Sunday, 16 March 2025 10:09:53 CET',
   apiEndpoint: 'https://richardeigenmann.github.io/Rezeptsammlung/recipesArray.json',
   appVersion: require('../../package.json').version,
   angularVersion: require('../../package.json').dependencies['@angular/core'],
