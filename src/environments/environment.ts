743dae6eb8802e69658b5bbcae5910560c291624
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-06-03 18:13:17 +0200

    Udate build timestamp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ declare const require: any;
 
 export const environment = {
   production: false,
-  buildTimeStamp: 'Sunday, 16 March 2025 10:15:43 CET',
+  buildTimeStamp: 'Tuesday, 03 June 2025 18:10:52 CEST',
   apiEndpoint: 'https://richardeigenmann.github.io/Rezeptsammlung/recipesArray.json',
   appVersion: require('../../package.json').version,
   angularVersion: require('../../package.json').dependencies['@angular/core'],
