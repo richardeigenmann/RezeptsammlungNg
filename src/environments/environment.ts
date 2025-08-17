66aea510983d859d4686293f92a3d86229ff8d34
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-08-18 01:14:20 +0200

    Update build timestamp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: false,
-  buildTimeStamp: 'Monday, 18 August 2025 00:00:13 CEST',
+  buildTimeStamp: 'Monday, 18 August 2025 01:14:04 CEST',
   apiEndpoint: 'https://richardeigenmann.github.io/Rezeptsammlung/recipesArray.json',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
