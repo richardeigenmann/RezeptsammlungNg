246b5605478c3c33bab673f27fdf1850ce8eb0cf
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-08-20 00:13:06 +0200

    Get rid of unused incorrect URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   production: true,
   buildTimeStamp: 'Monday, 18 August 2025 01:14:04 CEST',
-  apiEndpoint: 'https://richardeigenmann.github.io/Rezeptsammlung/recipesArray.json',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
   bootstrapVersion: packageInfo.dependencies['bootstrap'],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   production: false,
   buildTimeStamp: 'Monday, 18 August 2025 01:14:04 CEST',
-  apiEndpoint: 'https://richardeigenmann.github.io/Rezeptsammlung/recipesArray.json',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
   bootstrapVersion: packageInfo.dependencies['bootstrap'],
