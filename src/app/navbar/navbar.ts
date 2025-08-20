25245454a74c5e84a3adba1c72a1166caa6e7e9c
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-08-20 22:29:39 +0200

    Turn the search field into an Angular Reactive Form

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -4,12 +4,15 @@ import { FilterService } from '../services/filter';
 import { Router, RouterLinkActive, RouterLink } from '@angular/router';
 import { Menuabout } from '../menuabout/menuabout';
 import { EncodeURI } from '../shared/encodeUri';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators'; 
 
+// TODO: This should probably use an Angular Template Form or a Reactive Form
 @Component({
     selector: 'pm-navbar',
     templateUrl: './navbar.html',
     styleUrls: [],
-    imports: [RouterLinkActive, RouterLink, Menuabout, EncodeURI],
+    imports: [RouterLinkActive, RouterLink, Menuabout, EncodeURI, ReactiveFormsModule],
     standalone: true,
 })
 
@@ -17,6 +20,7 @@ export class Navbar implements OnInit {
   private _categoriesService = inject(CategoriesService);
   private _filterService = inject(FilterService);
   private router = inject(Router);
+  searchInput= new FormControl('');
 
 
   categoriesPivot = new Map<string, Map<string, number>>();
@@ -28,6 +32,15 @@ export class Navbar implements OnInit {
         this.categoriesPivot = categories;
         this.bindedCategoriesPivotKeys = Array.from(this.categoriesPivot.keys());
       } );
+
+    // Subscribe to valueChanges on the FormControl
+    this.searchInput.valueChanges.pipe(
+      debounceTime(100), // wait for 100ms pause in events
+      distinctUntilChanged() // only emit if value is different from previous
+    ).subscribe(searchTerm => {
+      // Announce the search term to the service
+      this._filterService.announceSearch(searchTerm);
+    });
   }
 
   getCategoryTypeValues(categoryType: string): string[] {
@@ -37,11 +50,5 @@ export class Navbar implements OnInit {
   onSearchClick() {
     this.router.navigate(['/recipes']);
   }
-
-  search(event: Event): void {
-    const searchTerm = (event.target as HTMLInputElement).value;
-    this._filterService.announceSearch(searchTerm);
-   }
-
 
 }
