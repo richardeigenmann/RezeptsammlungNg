c7623a96e3b430838b06f7a69ed0987f9e49d679
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-08-20 22:37:24 +0200

    Upgrade to Angular 20.2.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: false,
-  buildTimeStamp: 'Monday, 18 August 2025 01:14:04 CEST',
+  buildTimeStamp: 'Wednesday, 20 August 2025 22:35:07 CEST',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
   bootstrapVersion: packageInfo.dependencies['bootstrap'],
