64b613ff4bfb176e8a0585c9af84abf07067256f
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-08-20 23:01:46 +0200

    Push a new release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
-  buildTimeStamp: 'Wednesday, 20 August 2025 22:35:07 CEST',
+  buildTimeStamp: 'Wednesday, 20 August 2025 23:01:18 CEST',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
   bootstrapVersion: packageInfo.dependencies['bootstrap'],
