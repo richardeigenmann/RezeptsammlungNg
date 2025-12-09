38b9c08911461aa69a2da7a54e7cd6a0ae70d265
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-12-09 21:07:55 +0100

    Push a new version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
-  buildTimeStamp: 'Sunday, 30 November 2025 11:52:58 CET',
+  buildTimeStamp: 'Tuesday, 09 December 2025 21:07:11 CET',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
   bootstrapVersion: packageInfo.dependencies['bootstrap'],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: false,
-  buildTimeStamp: 'Sunday, 30 November 2025 11:52:58 CET',
+  buildTimeStamp: 'Tuesday, 09 December 2025 21:07:11 CET',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
   bootstrapVersion: packageInfo.dependencies['bootstrap'],
