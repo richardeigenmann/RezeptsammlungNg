31ff3c37eaaa1b7c192a5f7051bb97b0a17106fd
Richard Eigenmann <richard.eigenmann@gmail.com>
2026-02-11 20:41:39 +0100

    Deploy latest version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
-  buildTimeStamp: 'Tuesday, 09 December 2025 21:07:11 CET',
+  buildTimeStamp: 'Wednesday, 11 February 2026 20:40:46 CET',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
   bootstrapVersion: packageInfo.dependencies['bootstrap'],
