27d0b227b16a7258f4ff692fff9c015020db609f
Richard Eigenmann <richard.eigenmann@gmail.com>
2026-05-09 09:55:38 +0200

    Deploy new version without CVE-2026-6322

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: false,
-  buildTimeStamp: 'Thursday, 07 May 2026 15:30:12 CEST',
+  buildTimeStamp: 'Saturday, 09 May 2026 09:54:09 CEST',
   appVersion: packageInfo.version,
   angularVersion: packageInfo.dependencies['@angular/core'],
   bootstrapVersion: packageInfo.dependencies['bootstrap'],
